e0ade77768611d32f6cfc7aefd1bf103defe6aa9
Foysal Ahmed <foysalxahmed@gmail.com>
2024-07-27 07:48:30 +0600

    Refactor color variables

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import ThemeSwitcher from '@/components/Common/ThemeSwitcher'
 
 export const Navbar = () => {
   return (
-    <div className='bg-neutral-0 py-4 shadow-sm dark:border-b dark:border-b-neutral-30'>
+    <div className='bg-neutral-0 py-4 shadow-sm'>
       <div className='container'>
         <div className='flex justify-end'>
           <ThemeSwitcher />
